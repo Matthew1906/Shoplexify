c6e2a2ed70e3548fe65fb7a27be45c79a3c364ba
matthew adrianus <matthewadrianus@gmail.com>
2022-05-11 18:37:41 +0700

    Slight fix

--- onlineShop/models/__init__.py
+++ onlineShop/models/__init__.py
@@ -2,5 +2,5 @@ from .userModel import User
 from .productModel import Product 
 from .categoryModel import Category, ProductCategory
 from .orderModel import Order
-from .reviewModel import Review
+from .reviewModel import ProductReview
 from .transactionModel import Transaction, TransactionDetail
