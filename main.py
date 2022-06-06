f294ac13f82e48e835b1409b655968ad9bb8abbb
matthew adrianus <matthewadrianus@gmail.com>
2022-06-06 11:00:47 +0700

    Rename folder

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from onlineShop import create_app
+from app import create_app
 from os import environ
 
 # Deployment
 app = create_app()
 port = int(environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
+
