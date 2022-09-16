8e05b9f6b3b091d50a382a15b4afe3682c043750
Matthew1906 <matthew.mulyono001@binus.ac.id>
2022-09-16 21:12:34 +0700

    Bug Fix Notification System (2)

--- app/utils/notifications.py
+++ app/utils/notifications.py
@@ -3,7 +3,8 @@ from sqlalchemy import and_, or_
 from ..models import Transaction
 
 def get_notifications(user):
-    transactions = Transaction.query.filter(and_(Transaction.user_id==user.id,
-        or_(Transaction.date == date.today(), Transaction.payment_status=='Unpaid')))\
+    transactions = Transaction.query.filter(or_(
+        and_(Transaction.user_id==user.id, Transaction.date == date.today()),\
+            and_(Transaction.user_id==user.id, Transaction.payment_status=='Unpaid')))\
                 .order_by(Transaction.date.desc())
     return [{"id":tr.id, "date":tr.date, "status": tr.payment_status=='Paid', "color":"text-success" if tr.payment_status=='Paid' else "text-danger"} for tr in transactions]
